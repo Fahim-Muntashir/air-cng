1a9a6d4cac89a559a995ad28f5a602b72af3ef07
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-16 19:51:21 +0600

    category box added with all device responsive

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,12 +1,17 @@
 import Container from "../Shared/Container";
 import { categories } from "./CategoriesData";
+import CategoryBox from "./CategoryBox";
 
 const Categories = () => {
   return (
     <Container>
       <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
         {categories.map((item) => (
-          <p>{item.label}</p>
+          <CategoryBox
+            label={item.label}
+            icon={item.icon}
+            key={item.label}
+          ></CategoryBox>
         ))}
       </div>
     </Container>
